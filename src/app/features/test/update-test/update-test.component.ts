ec76ac90b83d77fca7299cf788d8904b7319077b
shinymilotic <trungtin.mai1412@gmail.com>
2024-07-25 21:36:36 +0700

    update code

--- src/app/features/test/update-test/update-test.component.ts
+++ src/app/features/test/update-test/update-test.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
+import { TestResponse } from 'src/app/core/models/test/test-response.model';
 import { TestService } from 'src/app/core/services/test.service';
+import { ListErrorsComponent } from "../../../shared/list-errors.component";
+import { ApiError } from 'src/app/core/models/apierrors.model';
+import { ChoiceQuestion } from 'src/app/core/models/test/choicequestion.model';
 
 @Component({
   selector: 'app-update-test',
   standalone: true,
-  imports: [],
+  imports: [ListErrorsComponent],
   templateUrl: './update-test.component.html',
   styleUrl: './update-test.component.css'
 })
-export class UpdateTestComponent implements OnInit{
-
+export class UpdateTestComponent implements OnInit {
+  errors!: ApiError;
+  test: TestResponse = {
+    author: {
+      id: '',
+      email: '',
+      username: '',
+      bio: '',
+      image: ''
+    },
+    description: "",
+    questions: [],
+    title: "",
+  };
+  
   constructor(
     private readonly route: ActivatedRoute,
     private readonly testService: TestService,
@@ -28,8 +45,31 @@ export class UpdateTestComponent implements OnInit{
         })
       )
       .subscribe(({data}) => {
-        console.log(data);
+        this.test = data;
       });
   }
 
+  deleteQuestion(_t11: any) {
+    throw new Error('Method not implemented.');
+  }
+  isChoiceQuestion(_t11: any) {
+    throw new Error('Method not implemented.');
+  }
+  deleteAnswer(_t11: any,_t20: any) {
+    throw new Error('Method not implemented.');
+  }
+  addAnswer(_t11: any) {
+    throw new Error('Method not implemented.');
+  }
+  addEssayQuestion() {
+    throw new Error('Method not implemented.');
+  }
+  addQuestion() {
+    throw new Error('Method not implemented.');
+  }
+
+  asChoiceQuestion(qIndex: number): ChoiceQuestion {
+    const q = this.test.questions[qIndex] as ChoiceQuestion
+    return q;
+  }
 }
