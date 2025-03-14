c0260fe09029a32de77f63e38f2610a13c0a7a7a
shinymilotic <trungtin.mai1412@gmail.com>
2025-03-14 11:48:31 +0700

    update files

---
+++ src/app/components/management/tag/create-tag/create-tag.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-tag',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-tag.component.html',
+  styleUrl: './create-tag.component.css'
+})
+export class CreateTagComponent {
+
+}
---
+++ src/app/components/management/tag/update-tag/update-tag.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-tag',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-tag.component.html',
+  styleUrl: './update-tag.component.css'
+})
+export class UpdateTagComponent {
+
+}
